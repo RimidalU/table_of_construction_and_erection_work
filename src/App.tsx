22f99e04bd302cd05cfdaaef3a8cc433754a1469
Uladzimir Stankevich <stankevichuuladimiru@gmail.com>
2023-01-28 21:39:39 +0300

    feat: add body elements

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
-import './App.sass'
+import './app.sass'
+import Aside from './components/Aside/Aside'
+import Header from './components/Header/Header'
+import Main from './components/Main/Main'
 
 function App() {
-  return (
-    <div className="App">
-    </div>
-  )
+	return (
+		<div className='App'>
+			<Header />
+			<Aside />
+			<Main />
+		</div>
+	)
 }
 
 export default App
